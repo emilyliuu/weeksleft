aaff8bbc87b64200a62a2614cae7366a5f22a6ce
fordacious <fordyfor@hotmail.com>
2018-06-10 12:15:33 -0700

    added intentions

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
+USE_TEST_DATA = true;
+
+testData = function () {
+
+    var intentions = [];
+
+    for (var i = 0 ; i < 1317; i++) {
+        intentions[i] = "";
+    }
+
+    intentions[0] = "be born";
+    intentions[100] = "eat food";
+    intentions[1000] = "contemplate purple";
+    intentions[1314] = "live in the moment";
+
+    return {
+        startDate: new Date("03/25/1993"),
+        numYears: 100,
+        intentions: intentions
+    };
+
+}();
+
 function saveData (appState) {
     return JSON.stringify(appState);
 }
 
 function loadData (jsonString) {
     return JSON.parse(jsonString);
+}
+
+function displayIntentionForWeek(intention) {
+    if (intention.length > 0) {
+        alert(intention);
+    }
 }
 
 function submit() {
@@ -15,10 +44,16 @@ function submit() {
     document.getElementById("initial-input").remove();
     document.getElementById("main_grid").style.display="inline-block";
 
+    if (USE_TEST_DATA) {
+        renderLoop(testData);
+        return;
+    }
+
+    // TODO make intentions equal num weeks
     renderLoop({
         startDate: bdayDate,
         numYears: numYears,
-        currentIntention: ""
+        intentions: intentions
     });
 }
 
@@ -31,7 +66,7 @@ function numWeeks(dateA, dateB) {
 }
 
 // Returns HTML for squares based on the number of weeks passed in
-function renderSquares(bdayDate, numYears, currentTime) {
+function renderSquares(bdayDate, numYears, intentions, currentTime) {
     var deathDate = new Date(bdayDate);
     deathDate.setFullYear(deathDate.getFullYear() + parseInt(numYears));
         
@@ -44,29 +79,42 @@ function renderSquares(bdayDate, numYears, currentTime) {
     // TODO optimize by only generating and updated the ones we need to
     // TODO click events for goal creation
 
-    var html = '';
+    var html = $('<div/>');
     for (var year = 0; year < numYears; year++) {
-        html += '<div class="row">' + '<div class="rowElement yearNum">' + (year + 1) + '</div>';
+        rowDiv = $('<div class="row">' + '<div class="rowElement yearNum">' + (year + 1) + '</div>');
         for (var week = 0; week < 52; week++) {
-            var weekDiff = weeksToNow - (week + ((year + 1) * 52) + 1); // 1 - 52, 53 - 104 etc...
+            var weekIndex = week + (year * 52);
+            var weekDiff = weeksToNow - (weekIndex + 1); // 1 - 52, 53 - 104 etc...
             var weekSqureFillPercentage = Math.min(Math.max(0, weekDiff), 1); // Clamp to [0, 1]
 
-            html +=
-                '<div class="rowElement weekSquare">' +
-                    //(week + 1) +
-                    '<div class="weekSquareFill" style="width:' + (weekSqureFillPercentage * 100) + '%"' + '></div>' +
-                '</div>'
-            ;
+            weekSquareDiv = $('<div class="rowElement weekSquare"/>');
+
+            var hasIntention = weekIndex < intentions.length && intentions[weekIndex].length > 0;
+
+            if (hasIntention) {
+                weekSquareDiv.click(function (index) {
+                    return function () {
+                        displayIntentionForWeek(intentions[index]);
+                    };
+                }(weekIndex));
+            }
+
+            weekSquareFillDiv = $('<div class="weekSquareFill" style="width:' + (weekSqureFillPercentage * 100) + '%"' + '></div>');
+            
+            weekSquareFillDiv.toggleClass("intention", hasIntention);
+
+            weekSquareDiv.append(weekSquareFillDiv);
+            rowDiv.append(weekSquareDiv);
         }
-        html += '</div>'
+        html.append(rowDiv);
     }
 
     return html;
 }
 
 function renderLoop (appState) {
-    var squaresHTML = renderSquares(appState.startDate, appState.numYears, new Date());
-    document.getElementById("main_grid").innerHTML = squaresHTML;
+    var squares = renderSquares(appState.startDate, appState.numYears, appState.intentions, new Date());
+    $("#main_grid").html(squares);
 
     window.requestAnimationFrame(renderLoop.bind(this, appState));
 }
